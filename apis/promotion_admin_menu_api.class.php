9c1d8dc36ab7ba463fd14d01ec5309e115547b7a
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-07 14:37:34 +0800

    调整菜单顺序

--- apis/promotion_admin_menu_api.class.php
+++ apis/promotion_admin_menu_api.class.php
@@ -8,7 +8,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class promotion_admin_menu_api extends Component_Event_Api {
 	
 	public function call(&$options) {	
-		$menus = ecjia_admin::make_admin_menu('03_promotion', __('促销管理'), '', 3);
+		$menus = ecjia_admin::make_admin_menu('03_promotion', __('促销管理'), '', 6);
 		$submenus = array(
 			ecjia_admin::make_admin_menu('promotion_manage', __('促销商品'), RC_Uri::url('promotion/admin/init'), 1)->add_purview('promotion_manage'),
 		);
