4e0d695b89528270f1c98adf6e7a1d2f9f089273
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 11:16:09 +0800

    修改促销商品列表错误返回值

--- modules/admin/promotion/list_module.class.php
+++ modules/admin/promotion/list_module.class.php
@@ -14,24 +14,23 @@ class list_module extends api_admin implements api_interface {
         $priv = $this->admin_priv('goods_manage');
 
         if (is_ecjia_error($priv)) {
-            $privilege = 0;
-            return array('data' => array(), 'pager' => null, 'privilege' => $privilege);
-        }
+			return $priv;
+		}
 
         $status     = $this->requestData('status', '');
         $keywords   = $this->requestData('keywords', '');
-        $size = $this->requestData('pagination.count', 15);
-        $page = $this->requestData('pagination.page', 1);
+        $size       = $this->requestData('pagination.count', 15);
+        $page       = $this->requestData('pagination.page', 1);
 
         $filter = array(
-            'status'     => $status,
-            'keywords'  => $keywords,
-            'size'         => !empty($size) ? intval($size) : 15,
-            'page'         => !empty($page) ? intval($page) : 1,
+            'status'        => $status,
+            'keywords'      => $keywords,
+            'size'          => !empty($size) ? intval($size) : 15,
+            'page'          => !empty($page) ? intval($page) : 1,
         );
-        $result = RC_Model::Model('goods/goods_model')->promotion_list($filter);
-        $privilege = 3;
-        $data = array();
+        $result     = RC_Model::Model('goods/goods_model')->promotion_list($filter);
+        $privilege  = 3;
+        $data       = array();
 
         if (!empty($result['item'])) {
             foreach ($result['item'] as $key => $val) {
