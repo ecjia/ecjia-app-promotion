1f3018daa2183c382965ea8d79e0b194e132d8fd
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 13:33:14 +0800

    优化编辑促销商品API

--- modules/admin/promotion/manage_module.class.php
+++ modules/admin/promotion/manage_module.class.php
@@ -3,51 +3,49 @@ defined('IN_ECJIA') or exit('No permission resources.');
 
 /**
  * 促销商品添加编辑处理
- * @author 
+ * @author
  *
  */
 class manage_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
-		$this->authadminSession();
-		
-		$ecjia = RC_Loader::load_app_class('api_admin', 'api');
-		$priv = $ecjia->admin_priv('goods_manage');
-		if (is_ecjia_error($priv)) {
-			$privilege = 0;
-			return array('data' => array(), 'pager' => null, 'privilege' => $privilege);
-		}
-		
-		$goods_id = $this->requestData('goods_id', 0);
-		if ($goods_id <= 0) {
-			return new ecjia_error(101, '参数错误');
-		}
-		
-		$promotion_info = RC_Model::Model('goods/goods_model')->promote_goods_info($goods_id);
-		/* 多商户处理*/
-		if (isset($_SESSION['seller_id']) && $_SESSION['seller_id'] > 0 && $promotion_info['seller_id'] != $_SESSION['seller_id']) {
-			return new ecjia_error(8, 'fail');
-		}
-		
-		$promotion = array(
-			'goods_id'				=> $goods_id,
-			'is_promote'    		=> '1',
-			'promote_price' 		=> $this->requestData('promote_price'),
-			'promote_start_date'    => RC_Time::local_strtotime($this->requestData('start_time')),
-			'promote_end_date'      => RC_Time::local_strtotime($this->requestData('end_time')),
-		);
-		
-		/* 检查促销时间 */
-		if ($promotion['promote_start_date'] >= $promotion['promote_end_date']) {
-			return new ecjia_error('time_error', __('促销开始时间不能大于或等于结束时间'));
-		}
-		
-		RC_Model::Model('goods/goods_model')->promotion_manage($promotion);
 
-		RC_Loader::load_app_func('global', 'promotion');
-		assign_adminlog_content();
-		ecjia_admin::admin_log($promotion_info['goods_name'], 'edit', 'promotion');
-		return array();
-	}
+        $this->authadminSession();
+
+        $priv = $this->admin_priv('goods_manage');
+        if (is_ecjia_error($priv)) {
+            return $priv;
+        }
+
+        $goods_id = $this->requestData('goods_id', 0);
+        if ($goods_id <= 0) {
+            return new ecjia_error(101, '参数错误');
+        }
+
+        $promotion_info = RC_Model::Model('goods/goods_model')->promote_goods_info($goods_id);
+        /* 多商户处理*/
+        if (isset($_SESSION['seller_id']) && $_SESSION['seller_id'] > 0 && $promotion_info['seller_id'] != $_SESSION['seller_id']) {
+            return new ecjia_error(8, 'fail');
+        }
+
+        $promotion = array(
+            'goods_id'              => $goods_id,
+            'is_promote'            => '1',
+            'promote_price'         => $this->requestData('promote_price'),
+            'promote_start_date'    => RC_Time::local_strtotime($this->requestData('start_time')),
+            'promote_end_date'      => RC_Time::local_strtotime($this->requestData('end_time')),
+        );
+
+        /* 检查促销时间 */
+        if ($promotion['promote_start_date'] >= $promotion['promote_end_date']) {
+            return new ecjia_error('time_error', __('促销开始时间不能大于或等于结束时间'));
+        }
+
+        RC_Model::Model('goods/goods_model')->promotion_manage($promotion);
+
+        RC_Loader::load_app_func('global', 'promotion');
+        assign_adminlog_content();
+        ecjia_admin::admin_log($promotion_info['goods_name'], 'edit', 'promotion');
+        return array();
+    }
 }
 // end
