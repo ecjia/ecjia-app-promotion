5cb6959249261f2f3d987bf59c7e3d7e2ce15cc4
季鸿飞 <huangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 13:25:07 +0800

    优化删除促销商品API

--- modules/admin/promotion/delete_module.class.php
+++ modules/admin/promotion/delete_module.class.php
@@ -8,29 +8,27 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class delete_module extends api_admin implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
-    		
+
 		$this->authadminSession();
-		$ecjia = RC_Loader::load_app_class('api_admin', 'api');
-		$priv = $ecjia->admin_priv('goods_manage');
+		$priv = $this->admin_priv('goods_manage');
 		if (is_ecjia_error($priv)) {
-			$privilege = 0;
-			return array('data' => array(), 'pager' => null, 'privilege' => $privilege);
+			return $priv;
 		}
 		$id = $this->requestData('goods_id', '0');
 		if ($id <= 0) {
 			return new ecjia_error(101, '参数错误');
 		}
-		
+
 		$promotion_info = RC_Model::Model('goods/goods_model')->promote_goods_info($id);
 		/* 多商户处理*/
 		if (isset($_SESSION['seller_id']) && $_SESSION['seller_id'] > 0 && $promotion_info['seller_id'] != $_SESSION['seller_id']) {
 			return new ecjia_error(8, 'fail');
 		}
-		
+
 		if (empty($promotion_info)) {
 			return new ecjia_error(13, '不存在的信息');
 		}
-		
+
 		$goods_name = $promotion_info['goods_name'];
 		$result = RC_Model::Model('goods/goods_model')->promotion_remove($id);
 		RC_Loader::load_app_func('global', 'promotion');
